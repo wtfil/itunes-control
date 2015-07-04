229a74831bf72db09a1875ac94e8c9446d7487c1
Filatov Evgen <evgen.filatov@gmail.com>
2015-07-04 21:39:28 +0300

    source

---
+++ index.js
@@ -0,0 +1,27 @@
+var util = require('util');
+var applescript = require('applescript');
+
+function createPlaylist(name, cb) {
+	var script = util.format(
+		'tell application "iTunes" \n try \n delete user playlist "%s" \n end try \n end tell\n' +
+		'tell application "iTunes" to make new user playlist with properties {name:"%s"}',
+		name,
+		name
+	);
+	applescript.execString(script, cb);
+}
+
+function addToPlaylist(playlist, filename, cb) {
+	var script = util.format(
+		'set foo to POSIX file "%s" as alias \n' +
+		'tell application "iTunes" to add foo to playlist "%s"',
+		filename,
+		playlist
+	);
+	applescript.execString(script, cb);
+}
+
+module.exports = {
+	createPlaylist: createPlaylist,
+	addToPlaylist: addToPlaylist
+};
